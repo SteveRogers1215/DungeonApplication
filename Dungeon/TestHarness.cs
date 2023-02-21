05d51b514a949ee4610f3175ecd5aa58d65447cf
Steven Rogers <steverogers1215@outlook.com>
2023-02-21 14:24:45 -0600

    Wireframed a weapon and character classes

---
+++ Dungeon/TestHarness.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon
+{
+    internal class TestHarness
+    {
+        static void Main(string[] args)
+        {
+            //build and test functionality of our library
+            //build and test a weapon
+            //build and test a character - include CalcBlock(), CalchitChance(), CalcDamage()
+        }
+    }
+}
---
+++ DungeonLibrary/Weapon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    //Make it public
+    public class Weapon
+    {
+        //Fields
+        /*
+         * int for min damage (cannot be less than 0 or > max
+         * int for max damage (cannot be less than 0) - assign first in CTOR
+         * string for the name
+         * int for bonusHitChance
+         * bool isTwoHanded
+         */
+
+        //Props - 1 for each field
+
+        //Constructors(CTORS)
+        //1 Fully Qualified CTOR and 1 Unqualified CTOR if you want object initialization syntax
+
+        //Methods
+        //Nicely formatted ToString() override
+    }
+}
