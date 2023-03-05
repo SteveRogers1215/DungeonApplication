1e208cc9edfaf8f537de9b0a981e2ddf996dc82a
Steven Rogers <steverogers1215@outlook.com>
2023-03-05 05:39:37 -0600

    For whatever reason I lost a great deal of code in VS, but retained it in Git. Pulled Git Repo to finish unit test examples and reuploading

--- UnitTesting/UnitTesting.cs
+++ UnitTesting/UnitTesting.cs
@@ -7,48 +7,66 @@ namespace UnitTesting
 {
     public class UnitTesting
     {
+        
         [Fact]
-        public Monster DungeonFunctionTest1()
+        public void DungeonTestingMinDamageTests()
         {
-            #region TestParameters
-            //Arrange
             Xenomorph m1 = new(name: "Chestburster", maxLife: 10, hitChance: 6, block: 6, maxDamage: 7, minDamage: 3,
                description: "You aren't in space, so we can definetly hear you scream");
+            int expectedMinDamage = 0;
+            int actualMinDamage = 0;
+            expectedMinDamage = 3;
+            actualMinDamage = m1.MinDamage;
+            Assert.Equal(expectedMinDamage,actualMinDamage)
+                
+        }
+        [Fact]
+        public void DungeonTestingMinDamageTests2()
+        {
             Wyvern m2 = new(name: "Randy", maxLife: 15, hitChance: 8, block: 7, maxDamage: 12, minDamage: 5,
                 description: "Is it a worm...or a dragon?");
-            Saibaman m3 = new(name: "Greeny", maxLife: 20, hitChance: 8, block: 10, maxDamage: 10, minDamage: 2,
-                description: "Just add water...");
-            Grunbeld m4 = new(name: "Grunbeld", maxLife: 50, hitChance: 10, block: 10, maxDamage: 25, minDamage: 5,
-                description: "The Legendary Flame Dragon Knight Apostle!");
+            int expectedMinDamage = 0;
+            int actualMinDamage = 0;
+            expectedMinDamage = 5;
+            actualMinDamage = m2.MinDamage;
+            Assert.Equal(expectedMinDamage, actualMinDamage)
 
-            #region TestParameters
-            
-            #endregion
-
-
-            //pick one at random to place in dungeon room
-
-            //Act
-            List<Monster> monsters = new()
-            {
-                m1,m1,m1,m1,
-                m2,m2,m2,
-                m3,m3,
-                m4,
-            };
-            return monsters[new Random().Next(monsters.Count)];
-
-            #endregion
-
-
-
-            
 
         }
         [Fact]
-        public void DungeonTesting()
+        public void DungeonTestingMinDamageTests3()
         {
+            Saibaman m3 = new(name: "Greeny", maxLife: 20, hitChance: 8, block: 10, maxDamage: 10, minDamage: 2,
+                description: "Just add water...");
+            int expectedMinDamage = 0;
+            int actualMinDamage = 0;
+            expectedMinDamage = 2;
+            actualMinDamage = m3.MinDamage;
+            Assert.Equal(expectedMinDamage, actualMinDamage)
 
         }
+        [Fact]
+        public void DungeonTestingMinDamageTests4()
+        {
+            Grunbeld m4 = new(name: "Grunbeld", maxLife: 50, hitChance: 10, block: 10, maxDamage: 25, minDamage: 5,
+                description: "The Legendary Flame Dragon Knight Apostle!");
+            int expectedMinDamage = 0;
+            int actualMinDamage = 0;
+            expectedMinDamage = 5;
+            actualMinDamage = m4.MinDamage;
+            Assert.Equal(expectedMinDamage, actualMinDamage)
+        }
+        [Fact]
+        public void DungeonTestingMinDamageTests5()
+        {
+            DemonKing m6 = new("Demon King", 75, 25, 17, 50, 25, "Sometimes its better to just run...");
+            int expectedMinDamage = 0;
+            int actualMinDamage = 0;
+            expectedMinDamage = 25;
+            actualMinDamage = m6.MinDamage;
+            Assert.Equal(expectedMinDamage, actualMinDamage)
+
+        }
+
     }
 }
